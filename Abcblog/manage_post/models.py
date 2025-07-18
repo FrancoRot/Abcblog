36418f3d4ee93a9c0bd2e7e9a17a6785b942f1f1
zangano <franco.rotella@hotmail.com>
2025-07-18 17:05:53 +0200

    creacion de modelos para category, article y status en postgreSQL

---
+++ Abcblog/manage_post/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User= get_user_model ()
+
+#categoria
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(blank=False, null=True)
+    slug = models.SlugField(unique=True, max_length=40)
+    featured = models.BooleanField(default=False)
+    created= models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True) 
+    status= models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorias'
+   
+
+#articulo
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    introduction = models.TextField(max_length=500)
+    image = models.ImageField()
+    body = models.TextField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    created= models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True) 
+    status= models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+        ordering = ['-created']
+    
+    
+class Rating(models.Model):
+    value = models.FloatField()
+    description = models.CharField(max_length=255)
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created= models.DateTimeField(auto_now_add=True)
+    status= models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.user_id.username
+    
+    class Meta:
+        verbose_name = 'Rating'
+        verbose_name_plural = 'Ratings'
+        ordering = ['-created']
+    
+    
+    
